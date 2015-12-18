78ca04b7b25a1cf3ffece262384682eed98701b9
Simon de Haan <simon@praekeltfoundation.org>
2015-12-18 17:05:32 +0100

    load fixtures from CSV

---
+++ load_fixtures.py
@@ -0,0 +1,12 @@
+import csv
+import data
+
+data.connect()
+
+reader = csv.reader(open('fixtures.csv'))
+
+for node_id, ip_address, site_name in reader:
+    nsm, created = data.NodeSiteMeta.create_or_get(node_id=node_id)
+    nsm.ip_address = ip_address
+    nsm.site_name = site_name
+    nsm.save()
